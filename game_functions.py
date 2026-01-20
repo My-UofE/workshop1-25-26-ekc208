b8c5b18a3874a2575099c857b9eb8a7aa75af1f2
ekc208 <ekc208@exeter.ac.uk>
2026-01-20 13:14:23 +0000

    changes to game 2

--- game_functions.py
+++ game_functions.py
@@ -11,10 +11,13 @@ def check_higher_lower(current_val, next_val, user_input):
     if current_val> next_val:
         if user_input == "l":
             return True
+        else:
+            return False
     elif current_val < next_val:
         if user_input == "h":
             return True
-    return False
+        else:
+            return False
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
